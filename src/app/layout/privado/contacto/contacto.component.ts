a0b1336d0a2f5fb9082c39926384ac483c16925d
JeanCasquete <Yinrodriguez71@gmail.com>
2023-01-02 11:29:44 -0500

    Finalizacion

--- src/app/layout/privado/contacto/contacto.component.ts
+++ src/app/layout/privado/contacto/contacto.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-contacto',
   templateUrl: './contacto.component.html',
   styleUrls: ['./contacto.component.css']
 })
-export class ContactoComponent {
+export class ContactoComponent implements OnInit {
+
+ dataUser: any 
+ 
+  constructor(private afAuth: AngularFireAuth,private router: Router,private _snackBar: MatSnackBar) {}
+  ngOnInit(): void {
+    this.afAuth.currentUser.then(user => {
+      if(user && user.emailVerified) 
+      {
+        this.dataUser = user;
+
+      } else {
+        this.router.navigate(['./login'])
+        this.error("Tienes que iniciar sesion para acceder a la pagina");
+      }
+    })
+  }
+
+  logout() {
+    this.afAuth.signOut().then(()=> this.router.navigate(['/login']))
+  }
+
+  error(msj: string) {
+    this._snackBar.open(msj, 'Ok', {
+      duration:5000,
+      horizontalPosition:'center',
+      verticalPosition:'bottom',
+
+    }) 
+  }
+    
 
 }
--- src/app/layout/privado/permisos/permisos.component.ts
+++ src/app/layout/privado/permisos/permisos.component.ts
@@ -1,20 +1,100 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';  
-import xml2js from 'xml2js'; 
-import { map } from 'rxjs/operators'
-
-
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Router } from '@angular/router';
+import DocumentJson from 'src/assets/json/datos.json';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-permisos',
   templateUrl: './permisos.component.html',
-  styleUrls: ['./permisos.component.css']
+  styleUrls: ['./permisos.component.css'],
 })
-export class PermisosComponent {
-  items: any[]=[]; // Declaración de la variable "items"
+export class PermisosComponent implements OnInit {
+  @ViewChild('seleCa')
+  select!: ElementRef;
+  @ViewChild('seleNiv')
+  select2!: ElementRef;
+  @ViewChild('selePar')
+  select3!: ElementRef;
+  @ViewChild('selePer')
+  select4!: ElementRef;
 
-  constructor() {
+  items: any[] = []; // Declaración de la variable "items"
+  public myForm!: FormGroup;
+  dataUser: any;
+
+  constructor(
+    private router: Router,
+    private fb: FormBuilder,
+    private afAuth: AngularFireAuth,
+    private afs: AngularFirestore,
+    private _snackBar: MatSnackBar
+  ) {
     this.getXMLData();
+  }
+
+  ngOnInit(): void {
+    this.myForm = this.createMyForms();
+    this.afAuth.currentUser.then((user) => {
+      if (user && user.emailVerified) {
+        this.dataUser = user;
+      } else {
+        this.router.navigate(['./login']);
+      }
+    });
+  }
+
+  private createMyForms(): FormGroup {
+    return this.fb.group({
+      names: ['', [Validators.required]],
+    });
+  }
+
+  validateSelections(): boolean {
+    return (
+      this.select.nativeElement.value !== '0' &&
+      this.select2.nativeElement.value !== '0' &&
+      this.select3.nativeElement.value !== '0' &&
+      this.select4.nativeElement.value !== '0'
+    );
+  }
+
+  public agregardato() {
+    const nombre = this.myForm.value.names;
+    const optionsCa = this.select.nativeElement.options;
+    const carrera =
+      optionsCa[this.select.nativeElement.selectedIndex].textContent;
+
+    const optionsNiv = this.select2.nativeElement.options;
+    const nivel =
+      optionsNiv[this.select2.nativeElement.selectedIndex].textContent;
+
+    const optionsPar = this.select3.nativeElement.options;
+    const Paralelo =
+      optionsPar[this.select3.nativeElement.selectedIndex].textContent;
+
+    const optionsPer = this.select4.nativeElement.options;
+    const Permiso =
+      optionsPer[this.select4.nativeElement.selectedIndex].textContent;
+    if (this.myForm.invalid) {
+      return this.error('llena todos los datos ');
+    } else if (this.validateSelections()) {
+      this.afAuth.currentUser.then((user) => {
+        console.log(nombre, carrera, nivel, Paralelo,Permiso, user?.email);
+        this.afs.collection('Permisos').add({
+          nombre: nombre,
+          correo: user?.email,
+          carrera: carrera,
+          nivel: nivel,
+          Paralelo: Paralelo,
+          solicitud: Permiso,
+        });
+      });
+    } else {
+      this.error('Seleciona una opcion para cada apartado');
+    }
   }
 
   async getXMLData() {
@@ -22,13 +102,23 @@ export class PermisosComponent {
     const text = await response.text();
     const parser = new DOMParser();
     const xml = parser.parseFromString(text, 'text/xml');
-    this.items = Array.from(xml.querySelectorAll('item')).map(item => {
+    this.items = Array.from(xml.querySelectorAll('item')).map((item) => {
       return {
         name: item.getAttribute('id'),
-        label: item.textContent
-        
+        label: item.textContent,
       };
     });
   }
- 
+
+  logout() {
+    this.afAuth.signOut().then(() => this.router.navigate(['/login']));
+  }
+
+  error(msj: string) {
+    this._snackBar.open(msj, 'Ok', {
+      duration: 5000,
+      horizontalPosition: 'center',
+      verticalPosition: 'bottom',
+    });
+  }
 }
